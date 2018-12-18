734073e4b05347fb6587bd620bd5c77fb61f3c70
Herold Ramaoka <Herold@tangentsolutions.co.za>
2018-12-18 17:47:31 +0200

    created getEmployees method

--- Herold-InterviewAssignment/Controllers/EmployeeController.cs
+++ Herold-InterviewAssignment/Controllers/EmployeeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using HeroldInterviewAssignment.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Herold_InterviewAssignment.Controllers
 {
@@ -11,5 +14,35 @@ namespace Herold_InterviewAssignment.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        public EmployeeController()
+        {
+
+        }
+
+        [Route("employees")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    //var token = "2a3d1af2f3f6d1cddaa3012c1c465fcbdffa3678";
+                    client.BaseAddress = new Uri("http://staging.tangent.tngnt.co/api-auth/login/?next=/api/username=pravin.gordhan&username&password=pravin.gordhan");
+                    var response = await client.GetAsync($"api/employee/");
+                    
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var jsonData = JsonConvert.DeserializeObject<Employee>(stringResult);
+
+                    return Ok("success");
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Something went wrong: {httpRequestException.Message }");
+                }
+            }
+        }
     }
 }
---
+++ Herold-InterviewAssignment/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroldInterviewAssignment.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        //public bool IsActive { get; set; }
+        //public bool IsStaff { get; set; }
+
+    }
+}
